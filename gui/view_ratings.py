f96985a32f328f4998ff5d53237e694fc09bf642
Larry La <larryquocla@gmail.com>
2025-05-09 09:26:38 -0700

    chore: add todos

--- gui/view_ratings.py
+++ gui/view_ratings.py
@@ -1,9 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
-from pdfminer.high_level import extract_text
-import os
 
-# TODO: FOR ELIJAH
+# Right now, a user's ratings are fetching and passed into this GUI as ratings IF they select to view history.
+# TODO: (Elijah) - Cleanse the ratings input, and display it cleanly in a tkinker GUI (currently only plaintext), display the 5 latest or more if it isn't too messy.
 class ViewRatingsGUI:
     def __init__(self, master, username, ratings):
         self.master = master
